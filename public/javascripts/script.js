025ba5cfdbc1b56a6f444525f69318be3a997245
rrtigga <rrtigga@ucdavis.edu>
2015-10-10 23:05:51 -0700

    got it kinda working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,64 +9,63 @@
   					success: function (data) {
 
   						
-  						
 			            document.getElementById("artist").innerHTML = data.response.hits[0].result.title; 
 			            document.getElementById("title").innerHTML = "By " + data.response.hits[0].result.primary_artist.name; 
 
-
+			            var title = data.response.hits[0].result.title; 
+			            var name = data.response.hits[0].result.primary_artist.name; 
 
 			            $.ajax({
 						    url: "https://api.genius.com/songs/" + data.response.hits[0].result.id,
 						    type: "GET",
 						    headers: {"Authorization": "Bearer WJCDe9kbzfhPQnwzBXQcMw8JExlRMefEWN19V0elsOMNhriiJodaO9Ld6hQ2TZn9"},
 		  					success: function (data) {
-
-					        }
-						});
-
-			        }
-				});
-		    	
-
-		    	$.get(
-					"https://www.googleapis.com/youtube/v3/search", {
-						part: 'snippet', 
-						maxResults: 10,
-						order: 'relevance', 
-						q: data.response.hits[0].result.title + data.response.hits[0].result.primary_artist.name, 
-						type: 'video',
-						key: 'AIzaSyAAYhBMn77W-cQ35ub-7o8F960_03DzYr4'
-					}, 
-					function(data){
-						$.each(data.items, function(i, item) {
-							if(i == 0)
-							{
-								console.log(item);
-								var videoToPlay = "https://www.youtube.com/watch?v=" + item.id.videoId + "&autoplay=1"; 
-								var videoToPlay = videoToPlay.replace("watch?v=", "v/");
-
-
-								$.get(
-									"https://www.googleapis.com/youtube/v3/captions", {
+		  						console.log("success");
+		  						$.get(
+									"https://www.googleapis.com/youtube/v3/search", {
 										part: 'snippet', 
-										videoId: item.id.videoId,
+										maxResults: 10,
+										order: 'relevance', 
+										//q is the search query
+										q: title + " " + name  , 
+										type: 'video',
 										key: 'AIzaSyAAYhBMn77W-cQ35ub-7o8F960_03DzYr4'
 									}, 
 									function(data){
+										$.each(data.items, function(i, item) {
+											if(i == 0)
+											{
+												console.log(item);
+												var videoToPlay = "https://www.youtube.com/watch?v=" + item.id.videoId + "&autoplay=1"; 
+												var videoToPlay = videoToPlay.replace("watch?v=", "v/");
+
+
+												$.get(
+													"https://www.googleapis.com/youtube/v3/captions", {
+														part: 'snippet', 
+														videoId: item.id.videoId,
+														key: 'AIzaSyAAYhBMn77W-cQ35ub-7o8F960_03DzYr4'
+													}, 
+													function(data){
+													}
+												)
+												document.getElementById("VID").src = videoToPlay; 
+											}
+										});
 									}
-								)
-								document.getElementById("VID").src = videoToPlay; 
+								);
 							}
-						})
+						});
 					}
-				)
-
-
-		     },
+				});
+			},
 		    'stop': function() { pause(); }, 
 		    'go': function() { resume(); },
 		    'Drake if i': function() {  }
 		  };
+		    	
+
+		    	
 
 
 
@@ -95,3 +94,5 @@
 		  // Start listening.
 		  annyang.start({continuous: true });
 		}
+
+
